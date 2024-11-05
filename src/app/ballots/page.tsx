ffa8cd97c5a343290526be702bb438f0e216b640
iyz <iy.usa@outlook.com>
2024-11-04 23:47:48 -0500

    add pagination to image gallery, fix css issues

--- src/app/ballots/page.tsx
+++ src/app/ballots/page.tsx
@@ -8,13 +8,33 @@ export default function Page() {
     "/primary-ballots-with-signatures-replaced.pdf"
   );
 
+  const [pageNum, setPageNum] = useState(0);
+  const imagesPerPage = 6;
+
   const imageFolder = "/primary-ballots-with-signatures-replaced";
   const imageCount = 20; // Adjust this number to the actual count of images
 
+  const startIndex = pageNum * imagesPerPage;
+  const endIndex = Math.min(startIndex + imagesPerPage, imageCount);
+
+  const handleNext = () => {
+    if (endIndex < imageCount) {
+      setPageNum(pageNum + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (pageNum > 0) {
+      setPageNum(pageNum - 1);
+    }
+  };
+
   const imageSrcs = Array.from(
-    { length: imageCount },
+    { length: endIndex - startIndex },
     (_, index) =>
-      `${imageFolder}/primary-ballots-with-signatures-replaced -images-${index}.jpg`
+      `${imageFolder}/primary-ballots-with-signatures-replaced -images-${
+        startIndex + index
+      }.jpg`
   );
 
   return (
@@ -26,6 +46,22 @@ export default function Page() {
           {imageSrcs.map((src, index) => (
             <ImageDisplay key={index} src={src} alt={`Image ${index + 1}`} />
           ))}
+          <div className="ballotButtonParent">
+            <button
+              className="green-btn ballotButton"
+              onClick={handlePrevious}
+              disabled={pageNum === 0}
+            >
+              Previous
+            </button>
+            <button
+              className="green-btn ballotButton"
+              onClick={handleNext}
+              disabled={endIndex >= imageCount}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </div>
