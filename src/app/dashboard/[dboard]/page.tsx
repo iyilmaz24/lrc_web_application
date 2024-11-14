03839ad0dd89c0058a217a43ffd9c2aa4514fce7
iyz <iy.usa@outlook.com>
2024-11-14 11:05:54 -0500

    add metadata + favicon, dashboard switching on page

--- src/app/dashboard/[dboard]/page.tsx
+++ src/app/dashboard/[dboard]/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import DashboardSelection from "@/app/_components/DashboardSelection";
 
-export default function Page({ params }: any) {
+export default function Page({ params }: { params: { dboard: string } }) {
   const { dboard } = params;
 
   const dashboards = {
@@ -15,19 +15,20 @@ export default function Page({ params }: any) {
   const dashboardUrl = dashboards[dboard] || dashboards["general-2022"];
 
   return (
-    <>
-      <div className="green-bg flex flex-col justify-center items-center py-20 px-4 gap-4">
+    <main className="min-h-screen">
+      <div className="min-h-[125px] flex flex-col items-center justify-center green-bg">
+        <DashboardSelection greenBackground={true} />
+        {/* <div className="">{dboard}</div> */}
+      </div>
+
+      <div className="green-bg flex flex-col justify-center items-center py-8 px-4">
         <iframe
-          className="min-w-[100%] max-h-[90%]"
+          className="min-w-[95%] max-h-[90%]"
           src={dashboardUrl}
           width={`w-screen`}
           height={750}
         ></iframe>
       </div>
-
-      <div className="min-h-[125px] flex items-center justify-center green-bg">
-        <DashboardSelection greenBackground={true} />
-      </div>
-    </>
+    </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,9 @@ import WebsiteNavigationMenu from "../app/_components/Mobile/WebsiteNavigationMe
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LRCI Web Application",
-  description: "LeRoy Collins Institute",
+  title: "LeRoy Collins Institute Post Election Audit Project",
+  description:
+    "All Florida counties are required by law to complete a postelection audit. In this project we provide information to the public about the accuracy of Florida ballot counting by presenting the results of each county’s post-election audit and where available the ballot images.",
 };
 
 export default function RootLayout({
@@ -18,6 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title> {"LeRoy Collins Institute Post Election Audit Project"}</title>
+        <meta
+          name="description"
+          content={
+            "All Florida counties are required by law to complete a postelection audit. In this project we provide information to the public about the accuracy of Florida ballot counting by presenting the results of each county’s post-election audit and where available the ballot images."
+          }
+        />
+        <link rel="icon" href="/LRCI-Favicon.png" type="image/png" />
+      </head>
+
       <body className={inter.className}>
         <WebsiteNavigationBar />
 
