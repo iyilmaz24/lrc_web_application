64bf0ba21a94c782699b83dcf2dfbb68323f9ba3
iyz <iy.usa@outlook.com>
2024-11-18 16:08:21 -0500

    fix metadata bug - title not popping up w/ favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ import WebsiteNavigationMenu from "../app/_components/Mobile/WebsiteNavigationMe
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "LeRoy Collins Institute Post Election Audit Project",
-//   description:
-//     "All Florida counties are required by law to complete a postelection audit. In this project we provide information to the public about the accuracy of Florida ballot counting by presenting the results of each county’s post-election audit and where available the ballot images.",
-// };
+export const metadata: Metadata = {
+  title: "LeRoy Collins Institute Post Election Audit Project",
+  description:
+    "All Florida counties are required by law to complete a postelection audit. In this project we provide information to the public about the accuracy of Florida ballot counting by presenting the results of each county’s post-election audit and where available the ballot images.",
+};
 
 export default function RootLayout({
   children,
@@ -20,13 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title> {"LeRoy Collins Institute Post Election Audit Project"}</title>
-        <meta
-          name="description"
-          content={
-            "All Florida counties are required by law to complete a postelection audit. In this project we provide information to the public about the accuracy of Florida ballot counting by presenting the results of each county’s post-election audit and where available the ballot images."
-          }
-        />
         <link
           rel="icon"
           href="https://lrci-main-web-app.s3.us-east-1.amazonaws.com/LCI-Favicon.png"
