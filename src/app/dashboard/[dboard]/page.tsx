624a8420dab5844b8070b3eb8852215433da77bb
iyz <iy.usa@outlook.com>
2025-01-08 23:05:15 -0500

    change loading message for dashboard

--- src/app/dashboard/[dboard]/page.tsx
+++ src/app/dashboard/[dboard]/page.tsx
@@ -30,13 +30,27 @@ export default function Page({ params }: { params: { dboard: string } }) {
       <div className="green-bg flex flex-col justify-center items-center py-8 px-4 ">
         {dashboardLoading && (
           <div className="min-w-[90%] h-[90%] bg-gray-200 text-black flex flex-col items-center justify-center gap-12 absolute">
-            <div className="max-w-[80%] text-center">
-              Dashboard Also Available In Full Screen Using Button
-            </div>
             <div className="max-w-[80%] text-center font-bold">
               Dashboard Loading...
             </div>
             <LoadingSpinner />
+            <div className="max-w-[80%] text-center flex items-center gap-1">
+              For full screen dashboard - use button below
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
           </div>
         )}
 
@@ -45,7 +59,7 @@ export default function Page({ params }: { params: { dboard: string } }) {
           src={dashboardUrl}
           width={`w-screen`}
           height={750}
-          onLoad={() => setDashboardLoading(false)}
+          onLoad={() => setDashboardLoading(true)}
         ></iframe>
       </div>
 
